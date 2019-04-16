5dbe6269d63a5bc86395fbf59573fec31bd59a5d
Mutmut <briend.mathilde@gmail.com>
2019-04-16 18:08:31 +0200

    Add V1 of the game

--- app.rb
+++ app.rb
@@ -4,4 +4,33 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-binding.pry
+player1 = Player.new("Jamie")
+player2 = Player.new("Randall")
+
+while player1.life_points > 0 && player2.life_points > 0 do
+
+  puts "
+  -----------
+  "
+
+  puts "Voici l'état de chaque joueur :"
+  player1.show_state
+  player2.show_state
+
+  puts "
+  ---
+  "
+
+  puts "Passons au duel :"
+
+  player1.attacks(player2)
+  if player2.life_points <= 0 || player1.life_points <=0
+    break
+  end
+  player2.attacks(player1)
+
+end
+
+  puts "
+  -----------
+  "
--- lib/player.rb
+++ lib/player.rb
@@ -1,32 +1,35 @@
 require 'pry'
 
-class User
-  attr_accessor :age, :email
-  @@all_users = []
+class Player # je crée ma classe Player que je vais définir
 
-  def initialize(email, age)
-    @email = email
-    @age = age
-    @@all_users << self
+  attr_accessor :name, :life_points # je permets de consulter, lire et modifier simplement le nom et le niveau de vie
+
+  def initialize(name) # j'initialise les données qui doivent être ajoutées en même temps que la création du player
+    @name = name # le nom du joueur
+    @life_points = 10 # le niveau de vie de départ
   end
 
-  def self.all
-    @@all_users
+  def show_state # méthode qui affiche le niveau de vie actuel d'un joueur
+    puts "#{@name} a #{@life_points} points de vie"
   end
 
-  def self.find_by_email(email)
-    @@all_users.each do |user|
-
-      if user.email == email
-        return user
+  def gets_damage(number) # méthode qui soustrait les dommages infligés au niveau de vie actuel
+    @life_points = @life_points - number
+      if @life_points <=0
+        puts "#{@name} a succombé à ses blessures !"
       end
+  end
 
+  def attacks(player) # méthode qui permet à un joueur d'infliger des dommages à un autre joueur
+    puts "#{@name} attaque #{player.name}"
+    number = compute_damage
+    puts "Il lui inflige #{number} points de dommage"
+    player.gets_damage(number)
+
+  end
+
+    def compute_damage # méthode qui sort au hasard un chiffre entre 1 et 6
+       return rand(1..6)
     end
 
-    puts "aucun utilisateur n'a cet email"
-    return false
-  end
-
 end
-
-binding.pry
